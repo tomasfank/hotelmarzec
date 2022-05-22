3cb3cf30ac0bc6f7bbb56becd3f09106cef3a917
tomasfank <tomasfank@outlook.com>
2022-05-21 23:23:08 -0300

    cambios21/5/22

--- js/main.js
+++ js/main.js
@@ -1,66 +1,31 @@
+/* variables*/
+let totalSuma = 0;
+let item;
+let habitacionID;
+let carrito = [];
+const addButton = document.getElementById("reservar");
+const deleteButton = document.getElementById("borrar");
+const ulist = document.getElementsByClassName("ulist")[0];
+const total = document.getElementsByClassName("total")[0];
+
+
 /* clase constructora */ 
 class Habitacion{
-    constructor(tipo, precio, id){
-        this.nombre = tipo;
+    constructor(nombre, precio, id){
+        this.nombre = nombre;
         this.precio = precio;
         this.id = id;
     }
 };
 
-/* variables globales */
-let habitacionID;
-let carrito = [];
-
 /* tipos de habitaciones */ 
-const individual = new Habitacion("Habitación individual", 1000, 1);
+const individual = new Habitacion("Habitación individual", 800, 1);
 const matrimonial = new Habitacion("Habitación matrimonial", 2000, 2);
 const presidencial = new Habitacion("Suite presidencial", 5000, 3);
 
 /* guardamos arreglo */ 
 const habitaciones = [individual, matrimonial, presidencial];
 
-/* funcion de bienvenida */ 
-const bienvenida = ()=>{
-    alert("Bienvenido a Hotel Marzec");
-    let usuario = prompt("Ingrese su nombre:  ")
-    while(usuario === ""){
-        usuario = prompt("Ingrese su nombre:  ")
-    }
-    return usuario;
-};
 
-/* Función de visualización y elección de habitación */ 
-const mostrarHabitaciones = ()=>{
-    let texto = "";
-    habitaciones.forEach(hab=>{
-        texto +=`${hab.id}) ${hab.nombre} $${hab.precio}\n`;
-    })
-    let seleccionHabitacion = parseInt(prompt(`¿Qué habitación desea reservar?:\n${texto}\n`));
-    while(seleccionHabitacion < 1 || seleccionHabitacion > 3){
-        seleccionHabitacion =  parseInt(prompt(`Por favor seleccione una opción posible.\n¿Qué habitación desea reservar?:\n${texto}\n`));
-    };
-    return seleccionHabitacion;
-};
-
-/* recopilamos la información para meterla en la funcion carrito */ 
-const habitacionSeleccionada = (id)=>{
-    console.log(id)
-    let habFind = habitaciones.find((hab)=> 
-        hab.id === id
-    );
-};
-
-
-
-/* iniciamos el programa */ 
-let nombreUsuario = bienvenida();
-
-/* alert de bienvenida al usuario */ 
-alert(`Bienvenido ${nombreUsuario}`);
-
-/* Mostramos las habitaciones disponibles */ 
-habitacionID = mostrarHabitaciones();
-
-habitacionSeleccionada(habitacionID);
-
+/* ----- FUNCTION ADD ----- */
 
