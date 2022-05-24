a008eae8f7c98006584dc93b3c77a878d62e0ddb
tomasfank <tomasfank@outlook.com>
2022-05-23 22:18:17 -0300

    cambios23-05-2022

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
 /* variables*/
-let totalSuma = 0;
-let item;
-let habitacionID;
-let carrito = [];
-const addButton = document.getElementById("reservar");
-const deleteButton = document.getElementById("borrar");
-const ulist = document.getElementsByClassName("ulist")[0];
-const total = document.getElementsByClassName("total")[0];
-
+const reservarBoton = document.querySelector("#reservar-btn")
+const reservarBoton2 = document.querySelector("#reservar-btn2")
+const reservarBoton3 = document.querySelector("#reservar-btn3")
+const vaciarBoton = document.querySelector("#borrar-btn")
+const contenedorCarrito = document.querySelector(".carrito-lista")
 
 /* clase constructora */ 
 class Habitacion{
-    constructor(nombre, precio, id){
+    constructor(nombre, precio){
         this.nombre = nombre;
-        this.precio = precio;
-        this.id = id;
+        this.precio = precio;  
     }
 };
 
 /* tipos de habitaciones */ 
-const individual = new Habitacion("Habitación individual", 800, 1);
-const matrimonial = new Habitacion("Habitación matrimonial", 2000, 2);
-const presidencial = new Habitacion("Suite presidencial", 5000, 3);
+const individual = new Habitacion("Habitación individual", 800);
+const matrimonial = new Habitacion("Habitación matrimonial", 2000);
+const presidencial = new Habitacion("Suite presidencial", 5000);
 
-/* guardamos arreglo */ 
-const habitaciones = [individual, matrimonial, presidencial];
+/* boton reservar */
+reservarBoton.addEventListener("click", ()=> {
+    let html;
 
+    html=
+    `
+    <h3>${individual.nombre}</h3>
+    <p>Total= $${individual.precio}</p>
+    <img src="img/individual.jpg" alt="individual" width="120px" height="120px">
+    `
+    contenedorCarrito.innerHTML += html
+});
 
-/* ----- FUNCTION ADD ----- */
+reservarBoton2.addEventListener("click", ()=>{
+    let html
 
+    html=
+    `
+    <h3>${matrimonial.nombre}</h3>
+    <p>Total= $${matrimonial.precio}</p>
+    <img src="img/matrimonial.jpg" alt="matrimonial" width="120px" height="120px">
+    `
+    contenedorCarrito.innerHTML += html
+});
+
+reservarBoton3.addEventListener("click", ()=>{
+    let html
+
+    html=
+    `
+    <h3>${presidencial.nombre}</h3>
+    <p>Total= $${presidencial.precio}</p>
+    <img src="img/presidencial.jpg" alt="matrimonial" width="120px" height="120px">
+    `
+    contenedorCarrito.innerHTML += html
+})
+
+ /* boton borrar */ 
+ function vaciarCarrito(){
+     contenedorCarrito.innerHTML= "";
+ }
+
+ vaciarBoton.addEventListener("click", ()=>{
+    vaciarCarrito();
+ });
